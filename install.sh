b9bf323c679d30dbef7cf7c951149c45a47da6ea
Shane Auckland <shane.auckland@gmail.com>
2017-02-24 16:43:06 +0000

    Remove name checking, I don't intend to change my name

--- install.sh
+++ install.sh
@@ -21,38 +21,9 @@ mkdir -p ~/.gitshots
 
 bot "Hi. I'm going to make your OSX system better. But first, I need to configure this project based on your info so you don't check in files to github"
 
-fullname=`osascript -e "long user name of (system info)"`
+fullname="$DEFAULT_NAME"
 
-if [[ -n "$fullname" ]];then
-  lastname=$(echo $fullname | awk '{print $2}');
-  firstname=$(echo $fullname | awk '{print $1}');
-fi
-
-# me=`dscl . -read /Users/$(whoami)`
-
-if [[ -z $lastname ]]; then
-  lastname=`dscl . -read /Users/$(whoami) | grep LastName | sed "s/LastName: //"`
-fi
-if [[ -z $firstname ]]; then
-  firstname=`dscl . -read /Users/$(whoami) | grep FirstName | sed "s/FirstName: //"`
-fi
 email=`dscl . -read /Users/$(whoami)  | grep EMailAddress | sed "s/EMailAddress: //"`
-
-if [[ ! "$firstname" ]];then
-  response='n'
-else
-  echo -e "I see that your full name is $COL_YELLOW$firstname $lastname$COL_RESET"
-  read -r -p "Is this correct? [Y|n] " response
-fi
-
-if [[ $response =~ ^(no|n|N) ]];then
-  read -r -p "What is your first name? " firstname
-  read -r -p "What is your last name? " lastname
-fi
-fullname="$firstname $lastname"
-
-bot "Great $fullname, "
-
 if [[ ! $email ]];then
   response='n'
 else
@@ -79,18 +50,16 @@ running "replacing items in .gitconfig with your info ($COL_YELLOW$fullname, $em
 
 # test if gnu-sed or osx sed
 
-sed -i "s/$DEFAULT_NAME/$firstname $lastname/" .gitconfig > /dev/null 2>&1 | true
+sed -i 's/'$DEFAULT_EMAIL'/'$email'/' .gitconfig > /dev/null 2>&1 | true
 if [[ ${PIPESTATUS[0]} != 0 ]]; then
   echo
   running "looks like you are using OSX sed rather than gnu-sed, accommodating"
-  sed -i '' "s/$DEFAULT_NAME/$firstname $lastname/" .gitconfig;
   sed -i '' 's/'$DEFAULT_EMAIL'/'$email'/' .gitconfig;
   sed -i '' 's/'$DEFAULT_GITHUBUSER'/'$githubuser'/' .gitconfig;
   sed -i '' 's/'$DEFAULT_USERNAME'/'$(whoami)'/g' .zshrc;ok
 else
   echo
   bot "looks like you are already using gnu-sed. woot!"
-  sed -i 's/'$DEFAULT_EMAIL'/'$email'/' .gitconfig;
   sed -i 's/'$DEFAULT_GITHUBUSER'/'$githubuser'/' .gitconfig;
   sed -i 's/'$DEFAULT_USERNAME'/'$(whoami)'/g' .zshrc;ok
 fi
